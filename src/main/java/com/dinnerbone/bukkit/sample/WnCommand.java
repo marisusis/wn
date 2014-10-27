a583ab42a159296a3b01539daddad53261ef285d
spacegeek224 <marisusisme@gmail.com>
2014-10-27 17:04:52 -0400

    Fixed more errors

--- src/main/java/com/dinnerbone/bukkit/sample/WnCommand.java
+++ src/main/java/com/dinnerbone/bukkit/sample/WnCommand.java
@@ -5,20 +5,18 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class WnCommand implements CommandExecutor{
 
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	public boolean onCommand(final CommandSender sender, Command cmd, String label, String[] args) {
 
 		String wnp = "[WN] ";
 		// TODO Auto-generated method stub
 		if (cmd.getName().equalsIgnoreCase("wn")) {
 			
-			if (args[0].equalsIgnoreCase("")) {
-				sender.sendMessage(ChatColor.GOLD + wnp + ChatColor.DARK_AQUA + "Do /wn help for help.");
-			}
 			if (args[0].equalsIgnoreCase("help")) {
-				sender.sendMessage(ChatColor.GOLD + wnp + ChatColor.LIGHT_PURPLE + "Use /bukkit:help wnplugin for help.");
+				helpMessage(sender);
 			}
 				
 			else if (args[0].equalsIgnoreCase("hello")) {
@@ -32,10 +30,19 @@ public class WnCommand implements CommandExecutor{
 			else if (args[0].equalsIgnoreCase("crap")) {
 				sender.sendMessage(ChatColor.GOLD + wnp + ChatColor.MAGIC + "crap" + ChatColor.RESET + " = " + ChatColor.GRAY + "crap");
 			}
+			else {
+				helpMessage(sender);
+			}
 
 		}
 		
 		return false;
+	}
+	public void helpMessage(CommandSender sender) {
+		String wnp = "[WN] ";
+		sender.sendMessage(wnp + "--------[Help]--------" );
+		sender.sendMessage(wnp + "Use /wn help for help");
+		sender.sendMessage(wnp + "Use /wn hello to say hello");
 	}
 
 }
