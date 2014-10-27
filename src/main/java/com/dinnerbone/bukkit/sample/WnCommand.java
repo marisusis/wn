7f5e2cd49d0f26944db2fcbc8b74d7b17d5d3f5c
spacegeek224 <marisusisme@gmail.com>
2014-10-27 16:35:33 -0400

    Added a main /wn command

--- src/main/java/com/dinnerbone/bukkit/sample/WnCommand.java
+++ src/main/java/com/dinnerbone/bukkit/sample/WnCommand.java
@@ -6,27 +6,28 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class WnArgsCommand implements CommandExecutor{
+public class WnCommand implements CommandExecutor{
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-		String wnp = "[WitherNetwork]";
+		String wnp = "[WN] ";
 		// TODO Auto-generated method stub
 		if (cmd.getName().equalsIgnoreCase("wn")) {
+			sender.sendMessage(ChatColor.GOLD + wnp + ChatColor.DARK_AQUA + "Do /wn help for help.");
 			if (args[0].equalsIgnoreCase("help")) {
-				Bukkit.broadcastMessage(ChatColor.BOLD + wnp + ChatColor.LIGHT_PURPLE + "Use /bukkit:help wnplugin for help.");
+				sender.sendMessage(ChatColor.GOLD + wnp + ChatColor.LIGHT_PURPLE + "Use /bukkit:help wnplugin for help.");
 			}
 				
 			else if (args[0].equalsIgnoreCase("hello")) {
-				Bukkit.broadcastMessage(ChatColor.BOLD + wnp + ChatColor.GREEN + "Hello everyone!");
+				Bukkit.broadcastMessage(ChatColor.GOLD + wnp + ChatColor.GREEN + "Hello everyone!");
 			}
 				
 			else if (args[0].equalsIgnoreCase("welcome")) {
-				Bukkit.broadcastMessage(ChatColor.BOLD + wnp + ChatColor.RED + "Welcome everyone!");
+				Bukkit.broadcastMessage(ChatColor.GOLD + wnp + ChatColor.RED + "Welcome everyone!");
 			}
 				
 			else if (args[0].equalsIgnoreCase("crap")) {
-				Bukkit.broadcastMessage(ChatColor.BOLD + wnp + ChatColor.MAGIC + "crap" + ChatColor.RESET + " =" + ChatColor.GRAY + "crap");
+				sender.sendMessage(ChatColor.GOLD + wnp + ChatColor.MAGIC + "crap" + ChatColor.RESET + " = " + ChatColor.GRAY + "crap");
 			}
 
 		}
